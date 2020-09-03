4bab0f67a6563f197b992bde08d066e825e4c0b1
chan21252 <cuican@5460cc.com>
2020-09-03 17:36:20 +0800

    身份验证

--- Authentication/src/main/java/AuthenticationTest.java
+++ Authentication/src/main/java/AuthenticationTest.java
@@ -1,8 +1,39 @@
+import org.apache.shiro.SecurityUtils;
+import org.apache.shiro.authc.AuthenticationException;
+import org.apache.shiro.authc.UsernamePasswordToken;
+import org.apache.shiro.config.IniSecurityManagerFactory;
+import org.apache.shiro.mgt.SecurityManager;
+import org.apache.shiro.subject.Subject;
+import org.apache.shiro.util.Factory;
+import org.junit.Assert;
+import org.junit.Test;
+
 /**
- * AutenticationTest
+ * AuthenticationTest
  *
  * @author Chan
  * @since 2020/9/3
  */
-public class AutenticationTest {
+public class AuthenticationTest {
+    @Test
+    public void testJdbcRealm() {
+        Factory<SecurityManager> factory =
+                new IniSecurityManagerFactory("classpath:shiro-jdbc-realm.ini");
+        SecurityManager securityManager = factory.getInstance();
+        SecurityUtils.setSecurityManager(securityManager);
+
+        Subject subject = SecurityUtils.getSubject();
+
+        UsernamePasswordToken token = new UsernamePasswordToken("chan", "123456");
+
+        try {
+            subject.login(token);
+        } catch (AuthenticationException e) {
+            e.printStackTrace();
+        }
+
+        Assert.assertTrue(subject.isAuthenticated());
+
+        subject.logout();
+    }
 }
--- Authentication/src/main/java/realm/MyRealm.java
+++ Authentication/src/main/java/realm/MyRealm.java
@@ -1,9 +1,39 @@
 package realm;
+
+import org.apache.shiro.authc.*;
+import org.apache.shiro.realm.Realm;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 /**
  * MyRealm
- * 
+ *
  * @author Chan
  * @since 2020/9/3
  */
-public class MyRealm {
+public class MyRealm implements Realm {
+
+    private final static Logger log = LoggerFactory.getLogger(MyRealm.class);
+
+    public String getName() {
+        return "MyRealm";
+    }
+
+    public boolean supports(AuthenticationToken authenticationToken) {
+        return authenticationToken instanceof UsernamePasswordToken;
+    }
+
+    public AuthenticationInfo getAuthenticationInfo(AuthenticationToken authenticationToken) throws AuthenticationException {
+        String username = authenticationToken.getPrincipal().toString();
+        String password =new String((char[])authenticationToken.getCredentials());
+
+        if(!"chan".equals(username)) {
+            throw new UnknownAccountException(); //如果用户名错误
+        }
+        if(!"123456".equals(password)) {
+            throw new IncorrectCredentialsException(); //如果密码错误
+        }
+        //如果身份认证验证成功，返回一个AuthenticationInfo实现；
+        return new SimpleAuthenticationInfo(username, password, getName());
+    }
 }
